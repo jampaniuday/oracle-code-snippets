42003dbec322604f5e0702dce1e4fb3f98208c89
Jorden Allcock <jordenallcock789@gmail.com>
2018-04-17 08:18:30 +0100

    diagnostics_menu_entry_enable.sql
    Anonymous block created to update/insert values into fnd_profile_options for the two profile options responsible for enabling Diagnostics Menu Entry in Oracle Forms at user level.
    
    FND_PROFILE_OPTIONS are:
    
    1. FND_HIDE_DIAGNOSTICS
    2. DIAGNOSTICS
    
    NB. user will have to log out and back into Oracle Forms for changes to take effect.

---
+++ diagnostics_menu_entry_enable.sql
@@ -0,0 +1,57 @@
+DECLARE
+
+    l_level_name VARCHAR2(100);
+    l_level_value_name VARCHAR2(100);
+    l_level_value NUMBER;
+    
+    l_progress_flag BOOLEAN;
+
+begin
+
+  -- APPL, RESP or USER
+  l_level_name := 'USER';
+
+  -- Application, Responsibility or User Name
+  l_level_value_name := upper(&level_value);
+
+  if l_level_value = 'USER' THEN
+    
+    SELECT user_id
+    INTO l_level_value
+    FROM apps.fnd_user
+    WHERE upper(user_name) = l_level_value_name;
+    
+    l_progress_flag := TRUE;
+    
+  else
+    l_progress_flag := FALSE;
+    
+  end if;
+
+if l_progress_flag THEN
+
+    if fnd_profile.save(X_NAME => 'FND_HIDE_DIAGNOSTICS',
+                     X_VALUE => 'N',
+                     X_LEVEL_NAME => l_level_name,
+                     X_LEVEL_VALUE => l_level_value) then
+   
+        dbms_output.put_line('FND_HIDE_DIAGNOSTICS updated to N for ' || l_level_name || + ' ' || l_level_value_name);
+    else
+        dbms_output.put_line('Fail');
+    end if;
+ 
+    if fnd_profile.save(X_NAME => 'DIAGNOSTICS',
+                     X_VALUE => 'Y',
+                     X_LEVEL_NAME => l_level_name,
+                     X_LEVEL_VALUE => l_level_value) then
+   
+        dbms_output.put_line('DIAGNOSTICS updated to Y for ' || l_level_name || + ' ' || l_level_value_name);
+    else
+        dbms_output.put_line('Fail');
+    end if;
+ else
+    dbms_output.put_line('Cannot update FND_PROFILE_OPTIONS, information entered is invalid');
+ end if;
+ 
+ commit;
+end;
